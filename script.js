fec0b35d94886edc64ec3509c36751e52e2b2eb6
LDantasA <leonardo.dantas0@gmail.com>
2021-12-11 00:14:55 -0300

    Adicionado o botão para salvar a lista de tarefas no Local Storage. (Sim, está consertado! 🥳)

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ function initialize() {
   if (storage != null) {
     /** Source: https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-web-storage/b332393f-7548-4075-83e3-f632735efb95/conteudos/a69f590a-b7be-4821-959e-75204430d057/local-e-session-storage/6da4a8cf-1a42-47c9-b271-a4df5f2ba5a3?use_case=side_bar */
     let savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    let completes = JSON.parse(localStorage.getItem('completes'));
 
     for (let i in savedTasks) {
       let task = {
         task: savedTasks[i],
-        completed: false,
+        completed: completes[i],
       };
       tasks.push(task);
     }
@@ -27,6 +28,9 @@ function listTasks() {
     let element = document.createElement('li');
 
     element.innerText = tasks[i].task;
+    if (tasks[i].completed === true) {
+      element.classList.add('completed');
+    }
     list.appendChild(element);
     element.addEventListener('click', taskSelector);
     element.addEventListener('dblclick', taskComplete);
@@ -36,7 +40,6 @@ function listTasks() {
 function addTask(event) {
   event.preventDefault();
 
-  const list = document.getElementById('lista-tarefas');
   const task = document.getElementById('texto-tarefa');
 
   tasks.push({
@@ -86,10 +89,24 @@ function removeCompletes() {
   listTasks();
 }
 
+function saveTasks() {
+  let savedTasks = [];
+  let completes = [];
+
+  for (let i in tasks) {
+    savedTasks.push(tasks[i].task);
+    completes.push(tasks[i].completed);
+  }
+
+  localStorage.setItem('tasks', JSON.stringify(savedTasks));
+  localStorage.setItem('completes', JSON.stringify(completes));
+}
+
 window.onload = function () {
   initialize();
   listTasks();
   document.querySelector('form').addEventListener('submit', addTask);
   document.querySelector('#apaga-tudo').addEventListener('click', removeAll);
   document.querySelector('#remover-finalizados').addEventListener('click', removeCompletes);
+  document.querySelector('#salvar-tarefas').addEventListener('click', saveTasks);
 };
